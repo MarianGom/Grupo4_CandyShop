bbcccea2bb73abc09cd338e8b9e24282b00579ab
lucianafigue <mlfluli@gmail.com>
2023-11-12 19:56:33 -0300

    Register (HTML-CSS)

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.get('/carrito', (req, res) =>{
     res.sendFile(path.join(__dirname, '/views/productCart.html'));
 });
 
-/* Registro -> Rocio */
-app.get('/registrar', (req, res) =>{
+/* Registro -> Luciana */
+app.get('/register', (req, res) =>{
     res.sendFile(path.join(__dirname, '/views/register.html'));
 });
 
