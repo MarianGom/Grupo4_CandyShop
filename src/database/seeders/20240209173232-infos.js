9a1e0cd10ff89f676109ea4a7ed04947f1893af5
Roo <cherryrooma@gmail.com>
2024-02-15 22:31:54 -0300

    [EDIT] migrations (info error)

--- src/database/seeders/20240209173232-infos.js
+++ src/database/seeders/20240209173232-infos.js
@@ -1,25 +1,30 @@
 'use strict';
+const { faker } = require("@faker-js/faker");
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    async up(queryInterface, Sequelize) {
+        const infos = [];
+            Array(20)
+                .fill(0)
+                .forEach((_, i) => {        
+                    const randomInfo = {
+                        id: i + 1,
+                        valorEnerg: faker.number.int(100),
+                        porcion: faker.commerce.department(),
+                        proteina: faker.number.int(100),
+                        /* sodio: faker.number.int(100),
+                        grasaTotal: faker.number.int(100),
+                        grasaSaturada: faker.number.int(100),
+                        grasaTrans: faker.number.int(100),
+                        fibraAlim: faker.number.int(100), */
+                        otros: faker.commerce.productDescription()
+                    };
+                    infos.push(randomInfo);
+                })
+        await queryInterface.bulkInsert("infos", infos);
+    },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    async down (queryInterface, Sequelize) {
+        await queryInterface.bulkDelete('infos', null, {});
+    }
 };
